9b4bd80be64cf98fa2399b85f6acc2890ef69107
nnakanghuy <huypk0607@gmail.com>
2026-09-20 10:30:19 +0700

    Time: 0 ms (100%), Space: 19.2 MB (91.48%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        arr = []
-        for i in range(len(s)):
-            arr.append(s[i])
-            if(len(arr)>=2):
-                if(arr[-2] == "[" and arr[-1] == "]"):
-                    arr.pop()
-                    arr.pop()
-                elif (arr[-2] == "(" and arr[-1] == ")"):
-                    arr.pop()
-                    arr.pop()
-                elif (arr[-2] == "{" and arr[-1] == "}"):
-                    arr.pop()
-                    arr.pop()
-        if(arr == []):
-            return True
-        return False
+        stack = []
+        lookup = {"(":")", "[":"]", "{":"}"}
+        for i in s:
+            if i in lookup:
+                stack.append(i)
+            elif len(stack)==0 or lookup[stack.pop()]!=i:
+                return False
+        return len(stack)==0
+
