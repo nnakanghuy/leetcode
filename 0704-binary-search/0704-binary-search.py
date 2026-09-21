a504aeb6913be8c8c3259131940dfece041f9db1
nnakanghuy <huypk0607@gmail.com>
2026-09-21 22:32:21 +0700

    Time: 0 ms (100%), Space: 20.6 MB (38.22%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        lp = 0 
+        rp = len(nums)-1
+        while lp<=rp:
+            mid = lp+((rp-lp)//2)
+            if(nums[mid]==target):
+                return mid
+            elif(nums[mid]<target):
+                lp = mid+1
+            else:
+                rp = mid-1
+        return -1
